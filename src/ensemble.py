055e18c9c7d96187d5c01ab707e054182254602d
Almaz <almazdautov1234523@gmail.com>
2023-11-24 15:05:56 +0300

    Added Ensemble of models

---
+++ src/ensemble.py
@@ -0,0 +1,18 @@
+import torch
+class EnsembleModel(torch.nn.Module):
+    """Ensemble of torch models, pass tensor through all models and average results"""
+
+    def __init__(self, models: list):
+        super().__init__()
+        self.models = torch.nn.ModuleList(models)
+
+    def forward(self, x):
+        result = None
+        for model in self.models:
+            y = model(x)
+            if result is None:
+                result = y
+            else:
+                result += y
+        result /= torch.tensor(len(self.models)).to(result.device)
+        return result
