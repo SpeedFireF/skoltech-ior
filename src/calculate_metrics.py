2c1f481c35ec8490896350dba146791f8a58b11b
Almaz <almazdautov1234523@gmail.com>
2023-11-24 03:05:16 +0300

    Tested unet500, unet3500

--- src/calculate_metrics.py
+++ src/calculate_metrics.py
@@ -1,7 +1,7 @@
 import torch
 
 def F1(groundtruth_mask, pred_mask):
-    intersect = torch.sum(pred_mask * groundtruth_mask, axis=[1, 2, 3])
-    total_sum = torch.sum(pred_mask, axis=[1, 2, 3]) + torch.sum(groundtruth_mask, axis=[1, 2, 3])
+    intersect = torch.sum(pred_mask * groundtruth_mask, dim=[1, 2])
+    total_sum = torch.sum(pred_mask, dim=[1, 2]) + torch.sum(groundtruth_mask, dim=[1, 2])
     dice = 2*intersect / total_sum
-    return dice.numpy()
+    return dice
